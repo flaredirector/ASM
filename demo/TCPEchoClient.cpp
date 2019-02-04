023bdeee27ca7786d1d7d82d21377ed7f1028648
Grant Savage <grantsavage1@me.com>
2019-02-04 09:05:08 -0700

    Small refactors

--- demo/TCPEchoClient.cpp
+++ demo/TCPEchoClient.cpp
@@ -27,38 +27,42 @@ const int RCVBUFSIZE = 32;    // Size of receive buffer
 
 int main(int argc, char *argv[]) {
   if ((argc < 2) || (argc > 3)) {     // Test for correct number of arguments
-    cerr << "Usage: " << argv[0] 
-         << " <Server> [<Server Port>]" << endl;
+    cerr << "Usage: " << argv[0] << " <Server> [<Server Port>]" << endl;
     exit(1);
   }
 
-  string servAddress = argv[1]; // First arg: server address
-  char echoString[RCVBUFSIZE] = "";// = argv[2];   // Second arg: string to echo
-  int echoStringLen = strlen(echoString);   // Determine input length
-  unsigned short echoServPort = (argc == 3) ? atoi(argv[2]) : 7;
+  string serverAddress = argv[1]; // First arg: server address
+  char echoString[RCVBUFSIZE] = ""; // Second arg: string to echo
+  int echoStringLength; // Input length
+  unsigned short echoServerPort = (argc == 3) ? atoi(argv[2]) : 7;
 
   try {
     // Establish connection with the echo server
-    TCPSocket sock(servAddress, echoServPort);
+    TCPSocket socket(serverAddress, echoServerPort);
   
     while (1) {
+      // Get message from user
       cout << "Enter a message: ";
       scanf("%[^\n]%*c", echoString);
-      echoStringLen = strlen(echoString);
+      echoStringLength = strlen(echoString);
+
       // Send the string to the echo server
-      sock.send(echoString, echoStringLen);
+      socket.send(echoString, echoStringLength);
     
+      // Prepare to receive message
       char echoBuffer[RCVBUFSIZE + 1];    // Buffer for echo string + \0
       int bytesReceived = 0;              // Bytes read on each recv()
       int totalBytesReceived = 0;         // Total bytes read
+
       // Receive the same string back from the server
       cout << "Received: ";               // Setup to print the echoed string
-      while (totalBytesReceived < echoStringLen) {
+      while (totalBytesReceived < echoStringLength) {
         // Receive up to the buffer size bytes from the sender
-        if ((bytesReceived = (sock.recv(echoBuffer, RCVBUFSIZE))) <= 0) {
+        if ((bytesReceived = (socket.recv(echoBuffer, RCVBUFSIZE))) <= 0) {
           cerr << "Unable to read";
           exit(1);
         }
+        
         totalBytesReceived += bytesReceived;     // Keep tally of total bytes
         echoBuffer[bytesReceived] = '\0';        // Terminate the string!
         cout << echoBuffer;                      // Print the echo buffer
--- demo/TCPEchoServer-Thread.cpp
+++ demo/TCPEchoServer-Thread.cpp
@@ -22,29 +22,29 @@
 #include <cstdlib>            // For atoi()  
 #include <pthread.h>          // For POSIX threads  
 
-const int RCVBUFSIZE = 32;
+const int RCVBUFSIZE = 32;    // 32 byte message buffer size
 
-void HandleTCPClient(TCPSocket *sock);     // TCP client handling function
+void HandleTCPClient(TCPSocket *socket);     // TCP client handling function
 void *ThreadMain(void *arg);               // Main program of a thread  
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {                 // Test for correct number of arguments  
+  if (argc != 2) {  // Test for correct number of arguments  
     cerr << "Usage: " << argv[0] << " <Server Port> " << endl;
     exit(1);
   }
 
-  unsigned short echoServPort = atoi(argv[1]);    // First arg:  local port  
+  unsigned short echoServerPort = atoi(argv[1]);    // First arg:  local port  
 
   try {
-    TCPServerSocket servSock(echoServPort);   // Socket descriptor for server  
+    TCPServerSocket serverSocket(echoServerPort);   // Socket descriptor for server  
   
     for (;;) {      // Run forever  
       // Create separate memory for client argument  
-      TCPSocket *clntSock = servSock.accept();
+      TCPSocket *clientSocket = serverSocket.accept();
   
       // Create client thread  
       pthread_t threadID;              // Thread ID from pthread_create()  
-      if (pthread_create(&threadID, NULL, ThreadMain, (void *) clntSock) != 0) {
+      if (pthread_create(&threadID, NULL, ThreadMain, (void *) clientSocket) != 0) {
         cerr << "Unable to create thread" << endl;
         exit(1);
       }
@@ -59,17 +59,16 @@ int main(int argc, char *argv[]) {
 }
 
 // TCP client handling function
-void HandleTCPClient(TCPSocket *sock) {
+void HandleTCPClient(TCPSocket *socket) {
   cout << "Handling client ";
-
   try {
-    cout << sock->getForeignAddress() << ":";
+    cout << socket->getForeignAddress() << ":";
   } catch (SocketException &e) {
     cerr << "Unable to get foreign address" << endl;
   }
 
   try {
-    cout << sock->getForeignPort();
+    cout << socket->getForeignPort();
   } catch (SocketException &e) {
     cerr << "Unable to get foreign port" << endl;
   }
@@ -77,24 +76,24 @@ void HandleTCPClient(TCPSocket *sock) {
 
   // Send received string and receive again until the end of transmission
   char echoBuffer[RCVBUFSIZE];
-  int recvMsgSize;
-  while ((recvMsgSize = sock->recv(echoBuffer, RCVBUFSIZE)) > 0) { // Zero means end of transmission
+  int receivedMessageSize;
+  while ((receivedMessageSize = socket->recv(echoBuffer, RCVBUFSIZE)) > 0) { // Zero means end of transmission
     string message = echoBuffer;
     cout << "Received Message: " << message << endl;
 
     // Echo message back to client
-    sock->send(echoBuffer, recvMsgSize);
+    socket->send(echoBuffer, receivedMessageSize);
   }
   // Destructor closes socket
 }
 
-void *ThreadMain(void *clntSock) {
+void *ThreadMain(void *clientSocket) {
   // Guarantees that thread resources are deallocated upon return  
   pthread_detach(pthread_self()); 
 
   // Extract socket file descriptor from argument  
-  HandleTCPClient((TCPSocket *) clntSock);
+  HandleTCPClient((TCPSocket *) clientSocket);
 
-  delete (TCPSocket *) clntSock;
+  delete (TCPSocket *) clientSocket;
   return NULL;
 }
